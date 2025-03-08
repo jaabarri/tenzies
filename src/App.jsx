f6a412209b1e71ac6c1faddfddfc5dd880307633
jaabarri <jaabarri1112@gmail.com>
2025-03-08 14:04:30 -0800

    Initial tenzies buttons css

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Die from "./components/Die"
+
+function App() {
+
+  return (
+    <main>
+      <div className="die-container">
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+      </div>
+
+    </main>
+  )
+}
+
+export default App
