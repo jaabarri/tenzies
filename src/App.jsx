6453f2f4e408d57d6033b0286d5da196782849de
jaabarri <jaabarri1112@gmail.com>
2025-03-09 13:11:02 -0700

    Initial random number generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import Die from "./components/Die"
+import { useState } from "react"
 
 function App() {
+
+
+  const [numbers, setNumbers] = useState(generateAllNewDice)
+
+
+  function generateAllNewDice() {
+    const new_arr = []
+    for(let i = 0; i < 10; i++){
+      new_arr.push(Math.floor(Math.random()*7))
+    }
+    return new_arr
+  }
+
+  const newDie = numbers.map((number) => <Die value={number}/>)
+
+
 
   return (
     <main>
       <div className="die-container">
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
+        {newDie}
       </div>
 
     </main>
